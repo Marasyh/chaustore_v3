87e6cba02b6b0fa95e356fb5993b11d74e60ddc8
marasy <marasyh@gmail.com>
2019-11-26 20:20:51 +0100

    first commit

--- src/Controller/Chaustore3Controller.php
+++ src/Controller/Chaustore3Controller.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Chaustore3Controller extends AbstractController
 {
-    /**
-     * @Route("/chaustore3", name="chaustore3")
-     */
-    public function index()
+
+    public function index(FormBuilderInterface $builder, array $options)
     {
+         $builder
+            ->add('field_name')
+            ->add('fiel_password')
+        ;
         return $this->render('chaustore3/index.html.twig', [
             'controller_name' => 'Chaustore3Controller',
         ]);
