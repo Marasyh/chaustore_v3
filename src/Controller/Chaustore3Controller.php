bdd2b27c4b44c07612faa2788548864ea6a7ac01
marasy <marasyh@gmail.com>
2019-12-01 14:45:33 +0100

    new add

--- src/Controller/Chaustore3Controller.php
+++ src/Controller/Chaustore3Controller.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Chaustore3Controller extends AbstractController
 {
-    /**
-     * @Route("/chaustore3", name="chaustore3")
-     */
+
     public function index()
     {
+        
+        ;
         return $this->render('chaustore3/index.html.twig', [
             'controller_name' => 'Chaustore3Controller',
         ]);
